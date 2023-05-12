2718d015faef4df6059f67849b0fdd529d9ec095
optimiseu <optimisedu@gmail.com>
2023-05-12 15:48:54 +0100

    basic rest server port setup

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,21 @@
+import express from 'express';
+import http from 'http';
+import bodyParser from 'body-parser';
+import cookieParser from 'cookie-parser';
+import compression from 'compression';
+import cors from 'cors'
+const PORT = 8080
+const app = express();
+app.use(cors({
+    credentials: true,
+}))
+app.use(cookieParser());
+app.use(compression());
+app.use(bodyParser.json());
+
+const server = http.createServer(app)
+server.listen(PORT, ()=>{
+    console.log(`'server is listening on port ${PORT}`)
+});
+
 console.log("thar she blows!")
